413581af970cd204aef0e4d39567c5b4874e8a71
pare <pare@ubun.(none)>
2016-08-10 23:20:14 +0800

    add c++ test

---
+++ cplusTest/test/testGit.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main(int argc, char* argv[])
+{
+	char* nameBuf = new char(255);
+	cout<<"please input you name:"<<endl;
+	cin>>nameBuf;
+	cout<<"hello :"<<nameBuf<<endl;
+
+	return 0;
+
+}
