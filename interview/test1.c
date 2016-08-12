3e2cffcce93da5f16a42af2b11d2456980f2423f
pareAml <pare_2011@126.com>
2016-08-12 19:33:47 +0800

    add function read temp from file

--- interview/test1.c
+++ interview/test1.c
@@ -1,17 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define FILE_PATH "city.txt"
+
 typedef struct  _CityLink {
 	char* CityName;
 	signed char CityTemp;
 	struct _CityLink* NextCity;
 } *CityLink;
 
+
 int main(int argc, char** argv)
 {
-	int a;
+	char inputBuf[127], *retBuf;
+	int a, len;
+	FILE *fd_city;
+	fd_city = fopen(FILE_PATH, "r");
+	if(fd_city == NULL)
+	{
+		perror("open file");
+		return -1;
+	};
+	
+	do {	
+		if((retBuf = fgets(inputBuf, 127, fd_city)) != NULL)
+			printf("%s", inputBuf);
+	}while( retBuf!= NULL );
+	
+
 	CityLink beijing;
 	beijing = (CityLink)malloc(sizeof(struct _CityLink));
 	beijing->CityTemp = 21;
 	printf("beijing temp is %d\n", beijing->CityTemp);
+	free(beijing);
+	fclose(fd_city);
 }
